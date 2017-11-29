6e8b94436a6d9d2588d489e8cfa10a91a91da8c5
James Rodgers <myfavoritenoisemaker@gmail.com>
2017-11-29 13:37:48 -0700

    Update chkSumCalc.js
    Documentation

--- bin/chkSumCalc.js
+++ bin/chkSumCalc.js
@@ -1,3 +1,5 @@
+//Calculate checksum for TAP handshaking, this is used to confirm part of the message didn't get dropped
+//more on checksum here http://fringe.davesource.com/Fringe/Hacking/Phreaking/Pagers/TAP_checksum.html
 chkSumCalc = function(pgr, txt){
     var r = "";
     var p = 0;
@@ -10,20 +12,15 @@ chkSumCalc = function(pgr, txt){
         t += txt.charCodeAt(i);
     }
     
-//    console.log("p " + p);
-//    console.log("t " + t);
     x = 31; //this part of chksum is constant becasue it is stx + 2 cr + etx
     sum = x + p + t;
-//    console.log("sum " + sum);
     d3 = 48 + sum - Math.floor(sum/16) * 16;
-//    console.log("d3 " + d3);
     sum = Math.floor(sum/16);
-//    console.log("sum " + sum);
     d2 = 48 + sum - Math.floor(sum/16) * 16;
     sum = Math.floor(sum/16);
     d1 = 48 + sum - Math.floor(sum/16) * 16;
     r = String.fromCharCode(d1, d2, d3);
-//    console.log("chkSum = " + r);
+//    console.log("chkSum = " + r); //uncomment this line to print chksum to console
     return r;
 };
 
